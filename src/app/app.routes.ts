8cab335c1ee1c2a32af2ae4e5a10aa2c566453a7
Saumitra Bagchi <saumitra.ku.cse@gmail.com>
2025-07-15 20:59:09 +0300

    feat: angular both from added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { MediaComponent } from './pages/media/media.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FormComponentComponent } from './pages/form-component/form-component.component';
 
 
 export const routes: Routes = [
   {
     path: '',
     component: MediaComponent,
-  }
+  },
+   {
+    path: 'form',
+    component: FormComponentComponent,
+  },
+   
 ];
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import { JupitarService } from '../../services/jupitar.service';
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule,MatSlideToggleModule],
+  imports: [MatButtonModule,MatSlideToggleModule,MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit{
+  currentrole:string=''
+ constructor(private jupitarService: JupitarService) {
+  
+ }
+  ngOnInit(): void {
+    
+  }
 }
